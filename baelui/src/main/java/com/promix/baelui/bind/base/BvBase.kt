4ae38f3a602be60eb1c2f69cbc9706c158fbe83d
promicx <phearom.dev@gmail.com>
2019-06-16 02:02:53 +0700

    1.2

--- baelui/src/main/java/com/promix/baelui/bind/base/BvBase.kt
+++ baelui/src/main/java/com/promix/baelui/bind/base/BvBase.kt
@@ -3,7 +3,7 @@ package com.promix.baelui.bind.base
 import androidx.databinding.BaseObservable
 
 abstract class BvBase<T>(var model: T) : BaseObservable() {
-    abstract fun getId(): String
+    abstract var id: String
 
     override fun equals(other: Any?): Boolean {
         return other is BvBase<*> && this.hashCode() == other.hashCode()
@@ -11,7 +11,7 @@ abstract class BvBase<T>(var model: T) : BaseObservable() {
 
     override fun hashCode(): Int {
         var result = model.hashCode()
-        result = 31 * result + getId().hashCode()
+        result = 31 * result + id.hashCode()
         return result
     }
 }
